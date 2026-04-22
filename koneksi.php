fe4e34dcea3b01b88778e3f2a9f42b09fcae45d5
Fahad-WSC <r3dalpha91@gmail.com>
2026-04-22 21:00:05 +0700

    Update deployment.yaml with PORT and env variables for Kubernetes

--- koneksi.php
+++ koneksi.php
@@ -1,12 +1,12 @@
 <?php
-$host = "db";
-$user = "root";
-$pass = "root";
-$db   = "db_sewa_properti";
+$host = getenv('DB_HOST') ?: 'localhost';
+$user = 'root';
+$pass = 'root';
+$db   = 'db_sewa_properti';
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-    die("Koneksi ke database gagal: " . mysqli_connect_error());
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
